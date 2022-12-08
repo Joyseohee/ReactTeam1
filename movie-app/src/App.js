91a7746f611baccc02684dfe8e18b197573e924c
joy <sh940311@gmail.com>
2022-12-08 20:55:09 +0900

    test

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,15 +1,34 @@
 import "./App.css";
-import RenderMovie from "./movie";
-import Mypage from "./pages/Mypage";
 import { Routes, Route, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Mypage from "./pages/Mypage";
+import Detail from "./pages/Detail";
+import Review from "./pages/Review";
+import Main from "./pages/Main";
+import RenderMovie from "./movie";
 
 function App() {
+  const [movie, setMovie] = useState([]);
+  const [like, setLike] = useState([]);
+
   return (
     <>
       <div className="App">
         <Routes>
-          <Route path="/mypage" element={<Mypage />} />
-          <Route path="/" element={<RenderMovie />} />
+          <Route
+            path="/mypage"
+            element={
+              <Mypage
+                movie={movie}
+                setMovie={setMovie}
+                like={like}
+                setLike={setLike}
+              />
+            }
+          />
+          <Route path="/detail:id" element={<Detail />} />
+          <Route path="/review/id" element={<Review />} />
+          <Route path="/" element={<Main like={like} setLike={setLike} />} />
         </Routes>
       </div>
     </>
