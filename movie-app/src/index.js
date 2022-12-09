72299b9dafcb6a2054199d76e19795773f3f5253
UserPark0390 <112944181+UserPark0390@users.noreply.github.com>
2022-12-09 10:20:28 +0900

    충돌 해결

--- movie-app/src/index.js
+++ movie-app/src/index.js
@@ -1,24 +1,21 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Provider } from 'react-redux';
-import store from './store.js'
-import './index.css';
-import App from './App';
+// import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-     <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    {/* <Provider store={store}> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    {/* </Provider> */}
   </React.StrictMode>
-
 );
 
 // If you want to start measuring performance in your app, pass a function
