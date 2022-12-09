6dfb7bdd3008de0abe1737d494b7208654f58763
joy <sh940311@gmail.com>
2022-12-09 09:43:23 +0900

    sh edit

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,9 +1,8 @@
-
 import "./App.css";
 import { useState } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MainMoive from './pages/Mainmovie';
+import "bootstrap/dist/css/bootstrap.min.css";
+import MainMoive from "./pages/Mainmovie";
 import Detail_joyTest from "./pages/Detail_joyTest";
 import Review_joyTest from "./pages/Review_joyTest";
 import Mypage from "./pages/Mypage";
@@ -11,8 +10,6 @@ import DetailPage from "./pages/DetailPage";
 import Detail from "./pages/Detail";
 import Main from "./pages/Main";
 import RenderMovie from "./movie";
-
-
 
 function App() {
   const [movie, setMovie] = useState([]);
@@ -33,13 +30,14 @@ function App() {
               />
             }
           />
-          <Route path="/detail:id" element={<Detail_joyTest />} />
-          <Route path="/review/id" element={<Review_joyTest />} />
-          <Route path="/" element={<MainMoive /> }/>
-        <Route path='/detail/:id' element={
-                <Detail />
-            }/>
-          <Route path="/" element={<Main like={like} setLike={setLike} />} />
+          <Route path="/de:id" element={<Detail_joyTest />} />
+          <Route path="/re:id" element={<Review_joyTest />} />
+          <Route path="/" element={<MainMoive />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route
+            path="/main"
+            element={<Main like={like} setLike={setLike} />}
+          />
         </Routes>
       </div>
     </>
