9228abf6635472df6eab0074dcff199acc916efe
3002291 <t3002291@gmail.com>
2022-12-11 16:08:09 +0900

    메인 swiper 적용

--- movie-app/src/pages/Mainmovie.js
+++ movie-app/src/pages/Mainmovie.js
@@ -1,82 +1,73 @@
 import { useEffect, useRef, useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, Navigation, Mousewheel } from "swiper";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-import {
-  Card,
-  CardActionArea,
-  CardMedia,
-  CardContent,
-  Typography,
-} from "@mui/material";
 
 import style from "./Mainmovie.module.css";
 import tmdbAPI from "../tmdbAPI";
 import Search from "./Search";
 import Loading from "../components/loading";
 import Header from "../components/header";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectCoverflow, Mousewheel, Navigation } from "swiper";
 
 function MainMoive() {
-  const API_IMAGEURL = "https://image.tmdb.org/t/p/w300"; // 영화 이미지 baseURL
-  const navigate = useNavigate();
-  const [movie, setMovie] = useState([]); // 가져올 영화 담을 배열
+    const API_IMAGEURL = "https://image.tmdb.org/t/p/w300"; // 영화 이미지 baseURL
+    const navigate = useNavigate();
+    const [movie, setMovie] = useState([]); // 가져올 영화 담을 배열
 
-  const [page, setPage] = useState(1); // axios param전달해줄 페이지
-  const [load, setLoad] = useState(false); // 로딩 성공/실패
-  const preventRef = useRef(true); // 중복 실행 방지
-  const observerRef = useRef(null); // observer Element(옵저버 타겟 대상 담을 곳)
+    const [page, setPage] = useState(1); // axios param전달해줄 페이지
+    const [load, setLoad] = useState(false); // 로딩 성공/실패
+    const preventRef = useRef(true); // 중복 실행 방지
+    const observerRef = useRef(null); // observer Element(옵저버 타겟 대상 담을 곳)
 
-  useEffect(() => {
-    AOS.init();
-  }, []);
+    useEffect(() => {
+        AOS.init();
+    }, []);
 
-  // 옵저버 생성 및 타겟(div) 지정
-  useEffect(() => {
-    const observer = new IntersectionObserver(obCallback, { threshold: 0.5 });
-    if (observerRef.current) observer.observe(observerRef.current);
-    return () => {
-      observer.disconnect();
+    // 옵저버 생성 및 타겟(div) 지정
+    useEffect(() => {
+        const observer = new IntersectionObserver(obCallback, { threshold: 0.5 });
+        if (observerRef.current) observer.observe(observerRef.current);
+        return () => {
+            observer.disconnect();
+        };
+    }, []);
+
+    // 옵저버 콜백함수
+    const obCallback = (entries) => {
+        const target = entries[0];
+        if (target.isIntersecting && preventRef.current) {
+            preventRef.current = false;
+            setPage((prev) => prev + 1);
+        }
     };
-  }, []);
 
-  // 옵저버 콜백함수
-  const obCallback = (entries) => {
-    const target = entries[0];
-    if (target.isIntersecting && preventRef.current) {
-      preventRef.current = false;
-      setPage((prev) => prev + 1);
-    }
-  };
+    // 페이지 값 변경될 때 마다 재렌더링
+    useEffect(() => {
+        getMovie();
+    }, [page]);
 
-  // 페이지 값 변경될 때 마다 재렌더링
-  useEffect(() => {
-    getMovie();
-  }, [page]);
+    // 영화 출력 함수
+    const getMovie = useCallback(async () => {
+        setLoad(true); // 로딩 시작
+        const movies = await tmdbAPI.get('movie/popular', { params: { page: `${page}` } });
+        if (movies.data) {
+            let temp = [...movie, ...movies.data.results];
+            setMovie(temp);
+            preventRef.current = true;
+        } else {
+            console.log("error");
+        }
+        setLoad(false); // 로딩 종료
+    }, [page]);
 
-  // 영화 출력 함수
-  const getMovie = useCallback(async () => {
-    setLoad(true); // 로딩 시작
-    const movies = await tmdbAPI.get("movie/popular", {
-      params: { page: `${page}` },
-    });
-    if (movies.data) {
-      let temp = [...movie, ...movies.data.results];
-      setMovie(temp);
-      preventRef.current = true;
-    } else {
-      console.log("error");
-    }
-    setLoad(false); // 로딩 종료
-  }, [page]);
+    const setData = (movie) => {
+        setMovie(movie);
+    };
 
-  const setData = (movie) => {
-    setMovie(movie);
-  };
 
   return (
     <>
@@ -84,8 +75,8 @@ function MainMoive() {
       <Header />
       {Search(setData)}
 
-      {/* 중간이미지 */}
-      {/* <Swiper
+            {/* 중간이미지 */}
+            <Swiper
                 effect={"coverflow"}
                 grabCursor={true}
                 centeredSlides={true}
@@ -100,59 +91,61 @@ function MainMoive() {
                 navigation={true} // 내비게이션 버튼
                 mousewheel={true} // 마우스 휠
                 modules={[EffectCoverflow, Mousewheel, Navigation]}
-                className="mySwiper"
+                className={style.mySwiper}
             >
-                <div className="container">
-                    {
-                        movie.map((movie, i) =>
-                                <SwiperSlide key={i}>
-                                    <div>
-                                        <img src={`${API_IMAGEURL}${movie.poster_path}`} />
-                                        <h4>{movie.title}</h4>
+                <div className={style.swipercontainer}>
+                  
+                        {
+                            movie.map((movie, i) =>
+                            <SwiperSlide>
+                                <div>
+                                    <img src={`${API_IMAGEURL}${movie.poster_path}`} />
+                                    <h4>{movie.title}</h4>
+                                </div>
+                            </SwiperSlide>
+                            )
+                        }
+                    
+                </div>
+            </Swiper>
+
+            {/* 영화렌더링 영역 */}
+            <div className={style.container}>
+                <div className={style.popular}>
+                    <h4 style={{ color: "white" }}>Popular</h4>
+                </div>
+
+                {
+                    movie &&
+                    <>
+                        {
+                            movie.map((movie, i) => {
+                                return (
+                                    <div key={i}>
+                                        <div className={style.movieCard}>
+                                            <img data-aos="slide-up" className={style.moviePoster} src={`${API_IMAGEURL}${movie.poster_path}`} onClick={() => { navigate(`detail/${movie.id}`) }} />
+                                            <div className={style.title}><h5>{movie.title}</h5></div>
+                                            {/* <div className={style.release_date}>{movie.release_date}</div> */}
+                                        </div>
                                     </div>
-                                </SwiperSlide>
-                        )
-                    }
-                </div>
-            </Swiper> */}
+                                );
+                            })
+                        }
 
-      {/* 영화렌더링 영역 */}
-      <div className={style.container}>
-        <div className={style.popular}>
-          <h4 style={{ color: "white" }}>Popular</h4>
-        </div>
 
-        {movie && (
-          <>
-            {movie.map((movie, i) => {
-              return (
-                <div key={i}>
-                  <div className={style.movieCard}>
-                    <img
-                      data-aos="slide-up"
-                      className={style.moviePoster}
-                      src={`${API_IMAGEURL}${movie.poster_path}`}
-                      onClick={() => {
-                        navigate(`detail/${movie.id}`);
-                      }}
-                    />
-                    <div className={style.title}>
-                      <h5>{movie.title}</h5>
-                    </div>
-                    {/* <div className={style.release_date}>{movie.release_date}</div> */}
-                  </div>
+                    </>
+                }
+                {
+                    load ? <Loading /> : <></>
+                }
+                <div>
+                    <div ref={observerRef} className="Observer"></div>
                 </div>
-              );
-            })}
-          </>
-        )}
-        {load ? <Loading /> : <></>}
-        <div>
-          <div ref={observerRef}>옵저버</div>
-        </div>
-      </div>
-    </>
-  );
+            </div>
+
+        </>
+    );
+
 }
 
 export default MainMoive;
