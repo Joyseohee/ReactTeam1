c71dc9dacd793a5bc4c5a3b15321c2e54bcee43c
Jmongle <110084761+Jmongle@users.noreply.github.com>
2022-12-11 15:46:08 +0900

    최신버전 pull 전 commit

--- movie-app/src/pages/Detail.js
+++ movie-app/src/pages/Detail.js
@@ -135,20 +135,6 @@ function Detail() {
                         </section>
                     </div>
                 </section>
-                
-                {/* <div className="player-wrapper">
-                <ReactPlayer
-                    className="react-player"
-                    url={`https://www.youtu.be/${movieKey}`} // 플레이어 url
-                    width="800px" // 플레이어 크기 (가로)
-                    height="500px" // 플레이어 크기 (세로)
-                    playing={true} // 자동 재생 on
-                    muted={true} // 자동 재생 on
-                    controls={true} // 플레이어 컨트롤 노출 여부
-                    light={false} // 플레이어 모드
-                    pip={true} // pip 모드 설정 여부
-                />
-                </div> */}
             </div>
                     <div style={{
                         backgroundColor: "white",
