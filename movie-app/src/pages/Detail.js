2bfd4e76e41f00c41912d4f4f63d7811b8d758de
Jmongle <110084761+Jmongle@users.noreply.github.com>
2022-12-12 18:08:18 +0900

    12/12 detail content

--- movie-app/src/pages/Detail.js
+++ movie-app/src/pages/Detail.js
@@ -4,6 +4,7 @@ import style from "./Detail.module.scss";
 import Loading from "../components/loading";
 import tmdbAPI from "../tmdbAPI";
 import Video from "./Video";
+import DetailContent from "./DetailContent";
 import ReactPlayer from "react-player";
 import ClickLikes from "../components/Detail/ClickLikes";
 import { Nav } from "react-bootstrap";
@@ -20,11 +21,11 @@ function Detail() {
   const API_IMAGEURL = "https://image.tmdb.org/t/p/w400";
 
   console.log(id.id);
-
   const [load, setLoad] = useState(null);
   const [video, setVideo] = useState([]);
   const [movieKey, setMoviekey] = useState();
   const navigate = useNavigate();
+
 
   const getDetailmv = async () => {
     setLoad(true); // 로딩 시작
@@ -60,6 +61,7 @@ function Detail() {
 
   useEffect(() => {
     getDetailmv();
+    //getActor();
   }, []);
 
   let [clickTab, setClickTab] = useState(0);
@@ -68,7 +70,6 @@ function Detail() {
     tmdbAPI
       .get(`movie/${id.id}/videos`, { params: { language: "en-US" } })
       .then((res) => {
-        console.log(111212);
         console.log(res.data.results);
         setVideo(res.data.results);
         setMoviekey(res.data.results[0].key);
@@ -76,8 +77,18 @@ function Detail() {
   }, []);
   console.log("movieKey: " + movieKey);
 
-  const [isOpen, setIsOpen] = useState(false);
+  // const [actor,setActor] = useState([]);
+  // const getActor = async () => {
+  //   setLoad(true); // 로딩 시작
+  //   const res = await tmdbAPI.get(`movie/${id.id}/credits`, { params: { language: "en-US" } });
+  //   if (res.data) {
+  //     setActor(res.data.cast);
+  //   } else {
+  //   }
+  //   setLoad(false); // 로딩 종료
+  // };
 
+  const [isOpen, setIsOpen] = useState(false);
   const onClickButton = () => {
     setIsOpen(!isOpen);
   };
@@ -89,44 +100,35 @@ function Detail() {
       ) : (
         <div className={style.back}>
             <div className={style.header}>
-            <div className={style.inner} style={{backgroundImage: `url(${API_IMAGEURL}${movie.backdrop_path})`, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
-                    <div className={style.img_wrapper}>
-                        <div className={style.box}>
-                            <img className={style.img} src={`${API_IMAGEURL}${movie.poster_path}`} />
-                            <span className={style.text}
-                        onClick={onClickButton}>트레일러 보기{isOpen&&<Video open={isOpen} movieKey={movieKey}
-                        style={style}
-                        onClose={() => {
-                          setIsOpen(false);
-                        }}/>}
-                      </span>
-                        </div>
-                        <section className={style.info_wrapper}>
-                            
-                                <span className={style.title}>{movie.title}</span><span>{movie.release_date}</span><br /><br />
-                                <span><img src={clock} className={style.runtime_img} /> </span><span className={style.runtime}>{movie.runtime} 분</span>
-                                <span><img src={percent} className={style.vote_average_img} /> </span><span className={style.vote_average}>{movie.vote_average}</span>
-                                <span><img src={like} className={style.vote_count_img} onClick={() => {
-                        storeLikes(); // 클릭 시 좋아요
-                      }}/> </span><span className={style.vote_count}>{movie.vote_count}</span><br /><br />
-                                <span className={style.tagline}>{movie.tagline}</span><br /><br />
-                                <span className={style.overview}>{movie.overview}</span>
-                            
-                        </section>
-                    </div>
-                </div>
-            </div>
-                    <div style={{
-                        backgroundColor: "white",
-                        display: "flex",
-                    }}
-                    onClick={() => {
-                        navigate(`/mypage/likes/${movie.id}`);
-                    }}
-                    >
-                    <ClickLikes />
-                    </div>
-          
+              <div className={style.inner} style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
+                      <div className={style.img_wrapper}>
+                          <div className={style.box}>
+                              <img className={style.img} src={`${API_IMAGEURL}${movie.poster_path}`} />
+                              <span className={style.text}
+                                onClick={onClickButton}>트레일러 보기{isOpen&&<Video open={isOpen} movieKey={movieKey}
+                                style={style}
+                                onClose={() => {
+                                  setIsOpen(false);
+                                }}/>}
+                              </span>
+                          </div>
+                          <section className={style.info_wrapper}>
+                              
+                                  <span className={style.title}>{movie.title}</span><span>{movie.release_date}</span><br /><br />
+                                  <span><img src={clock} className={style.runtime_img} /> </span><span className={style.runtime}>{movie.runtime} 분</span>
+                                  <span><img src={percent} className={style.vote_average_img} /> </span><span className={style.vote_average}>{movie.vote_average}</span>
+                                  <span><img src={like} className={style.vote_count_img} onClick={() => {
+                          storeLikes(); // 클릭 시 좋아요
+                        }}/> </span><span className={style.vote_count}>{movie.vote_count}</span><br /><br />
+                                  <span className={style.tagline}>{movie.tagline}</span><br /><br />
+                                  <span className={style.overview}>{movie.overview}</span>
+                              
+                          </section>
+                      </div>
+                  </div>
+            </div><br />
+
+          <div className={style.nav}>
           <Nav fill variant="tabs" defaultActiveKey="link-0">
             <Nav.Item>
               <Nav.Link onClick={() => {setClickTab(0);}} eventKey="link-0">상세정보</Nav.Link>
@@ -139,16 +141,26 @@ function Detail() {
           </Nav>
 
           <TabContent clickTab={clickTab} movies={movie} />
+          </div>
         </div>
       )}
     </>
   );
+  
 }
 
 function TabContent(props) {
   console.log(props.clickTab);
+  const movieId = props.movies.id;
+
+  console.log("movieId"+ props.movies.id)
+
   if (props.clickTab == 0) {
-    return <div style={{ color: "white" }}>{props.movies.original_title}</div>;
+    return (
+      <>
+        <DetailContent movieId={movieId}/>
+      </>
+    );
   }
   if (props.clickTab == 1) {
     return <div style={{ color: "white" }}>{props.movies.tagline}</div>;
