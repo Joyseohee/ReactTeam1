dfac2b9d4fcdade5017b2ef0240fdae8317dd2c7
Jmongle <110084761+Jmongle@users.noreply.github.com>
2022-12-15 14:19:34 +0900

    12/15 mg

--- movie-app/src/components/Detail/SeasonEpis.js
+++ movie-app/src/components/Detail/SeasonEpis.js
@@ -48,32 +48,35 @@ function SeasonEpis (props){
         <div>
             <h2 style={{ color: "white" }}>시즌</h2>
         <div className={style.container}>
-            {season.map((season, i) => {
+            {season.map((Season, i) => {
               return (
                 <div className={style.box}>
                 {
-                    season.poster_path == null?
+                    Season.poster_path == null?
                     <img className={style.img} src={png} /> :
-                    <img className={style.img_poster} src={`${API_IMAGEURL}${season.poster_path}`} />
+                    <img className={style.img_poster} src={`${API_IMAGEURL}${Season.poster_path}`} />
                 }
                     <section className={style.info}>
-                        <div className={style.name}>{season.name}</div>
-                        <span className={style.episode_count}>{season.episode_count}개의 에피소드&nbsp;&nbsp;&nbsp;</span>
+                        <div className={style.name}>{Season.name}</div>
+                        <span className={style.episode_count}>{Season.episode_count}개의 에피소드&nbsp;&nbsp;&nbsp;</span>
                         <button className={style.epibutton} onClick={()=>onClickButton(i)}>에피소드 보기</button><br /><br />
-                        {/* <div className={style.episode}>
-                            {isOpen && (
-                            <h2 style={{ color: "white" }}>{i}</h2>
-                            )}
-                        </div> */}
                             <div className={style.episode}>
-                            {isOpen[i] && (
-                            <Episodes open={isOpen} season={season} i={i} tvId={props.tvId}
-                                onClose={() => {
-                                setIsOpen(false);}}/>
-                            )}
+                                {
+                                    season[0].season_number == 0?<>
+                                    {isOpen[i] && (
+                                    <Episodes open={isOpen} season={Season} i={i} tvId={props.tvId}
+                                        onClose={() => {
+                                        setIsOpen(false);}}/>
+                                    )}</> :
+                                    <>{isOpen[i] && (
+                                        <Episodes open={isOpen} season={Season} i={i+1} tvId={props.tvId}
+                                            onClose={() => {
+                                            setIsOpen(false);}}/>
+                                        )}</>
+                                }
                             </div>
-                        <div className={style.air_date}>공개일 : {season.air_date}</div><br />
-                        <div className={style.overview}>{season.overview}</div>
+                        <div className={style.air_date}>공개일 : {Season.air_date}</div><br />
+                        <div className={style.overview}>{Season.overview}</div>
                     </section>
                 </div>
               );
